8d27ec8b17476c6bea41bb2d54575b1ec72ad670
Kamil Samigullin <kamil@samigullin.info>
2022-02-06 09:51:41 +0300

    dev: fix unbound var for GOBIN

--- draft/octomation/go-module/bin/lib/tools/tools.bash
+++ draft/octomation/go-module/bin/lib/tools/tools.bash
@@ -8,7 +8,7 @@ path="$(@root)/bin/${_os}/${_arch}"
 if [[ ":${PATH}:" != *":${path}:"* ]]; then
   export PATH="${path}:${PATH}"
 fi
-if [[ -z "${GOBIN}" ]]; then
+if [[ -z "${GOBIN+x}" ]]; then
   export GOBIN="${path}"
 fi
 
